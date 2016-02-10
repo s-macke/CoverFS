53bf32c8b47a6f1fc58bdefd2dd540c1c45d21a7
Sebastian Macke <sebastian@macke.de>
2016-02-10 19:19:17 +0000

    parallelized fragment checking tool

--- tests/checkfragment.cpp
+++ tests/checkfragment.cpp
@@ -5,6 +5,8 @@
 #include<fcntl.h>
 #include<assert.h>
 #include<sys/stat.h>
+#include<thread>
+#include<mutex>
 
 #define MAXSIZE 0xFFFFF
 
@@ -12,16 +14,18 @@
 
 typedef struct
 {
-	int fd;
-	char filename[256];
-	size_t size;
-	char data[MAXSIZE+2];
+    int fd;
+    char filename[256];
+    int size;
+    std::mutex mtx;
+    char data[MAXSIZE+2];
 } FSTRUCT;
 
 FSTRUCT files[10];
+int niter = 2000;
+int nfiles = 10;
 
 // ----------------------
-
 
 size_t fsize(int fd) 
 {
@@ -32,92 +36,126 @@ size_t fsize(int fd)
     return st.st_size;   
 }
 
-
-char data[MAXSIZE+2];
-
-int main()
+void Execute(int tid)
 {
-	for(int i=0; i<10; i++)
-	{
-		sprintf(files[i].filename, "tests%i.dat", i);
-		files[i].fd = open(files[i].filename, O_RDWR | O_CREAT | O_TRUNC);
-		files[i].size = 0;
-		memset(files[i].data, 0, MAXSIZE+1);
-	}
+    char *data = new char[MAXSIZE+2];
+    ssize_t retsize;
 
-	//while(1)
-	for(int iter=0; iter<2000; iter++)
-	{
-                ssize_t retsize;
-		int cmd = rand()%4;
-		int id = rand()%10;
-		int ofs = 0;
-		if (files[id].size > 0) ofs = rand() % files[id].size;
-		int newsize = rand()& MAXSIZE;
-                if (ofs+newsize > MAXSIZE) newsize = MAXSIZE - ofs - 1;
-		if (newsize < 0) newsize = 0;
+    //printf("thread %i:\n", tid);
+    for(int iter=0; iter<niter; iter++)
+    {
+        int cmd = rand()%4;
+        int id = rand()%10;
+        int ofs = 0;
+        files[id].mtx.lock();
+        if (files[id].size > 0) ofs = rand() % files[id].size;
+        int newsize = rand() & MAXSIZE;
+        if (ofs+newsize > MAXSIZE) newsize = MAXSIZE - ofs - 1;
+        if (newsize < 0) newsize = 0;
 
-		//printf("cmd=%i id=%i\n", cmd, id);
-		switch(cmd)
-		{
-			case 0: // Truncate
-				newsize = rand() & MAXSIZE;
-				printf("%5i: Truncate %i size=%i\n", iter, id, newsize);
-	                        if (newsize > files[id].size)
-				{
-					memset(&(files[id].data[ files[id].size ]), 0, newsize-files[id].size);
-				}
-				files[id].size = newsize;
-				if (ftruncate(files[id].fd, files[id].size) != 0)
-				{
-					printf("Error during truncate\n");
-					exit(1);
-				}
-			break;
+        //printf("cmd=%i id=%i\n", cmd, id);
+        switch(cmd)
+        {
+        case 0: // Truncate
+            newsize = rand() & MAXSIZE;
+            printf("tid %1i %5i: Truncate %i size=%i\n", tid, iter, id, newsize);
+            if (newsize > files[id].size)
+            {
+                memset(&(files[id].data[ files[id].size ]), 0, newsize-files[id].size);
+            }
+            files[id].size = newsize;
+            if (ftruncate(files[id].fd, files[id].size) != 0)
+            {
+                printf("Error during truncate\n");
+                exit(1);
+            }
+             break;
 
-			case 1: // write
-				printf("%5i: write %i ofs=%i size=%i\n", iter, id, ofs, newsize);
-				for(int i=0; i<newsize; i++)
-				{
-					files[id].data[ofs+i] = rand();
-				}
-				retsize = pwrite(files[id].fd, &(files[id].data[ofs]), newsize, ofs);
-				assert(retsize == newsize);
-				if (ofs+newsize > files[id].size) files[id].size = ofs+newsize;
-			break;
+        case 1: // write
+            printf("tid %1i %5i: write %i ofs=%i size=%i\n", tid, iter, id, ofs, newsize);
+            for(int i=0; i<newsize; i++)
+            {
+                files[id].data[ofs+i] = rand();
+            }
+            retsize = pwrite(files[id].fd, &(files[id].data[ofs]), newsize, ofs);
+            assert(retsize == newsize);
+            if (ofs+newsize > files[id].size) files[id].size = ofs+newsize;
+            break;
 
-			case 2: // read
-				printf("%5i: read %i ofs=%i size=%i\n", iter, id, ofs, newsize);
-		                if (ofs+newsize > files[id].size) newsize = files[id].size - ofs - 1;
-				retsize = pread(files[id].fd, &data, newsize, ofs);
-				assert(retsize == newsize);
-				for(int i=0; i<newsize; i++)
-				{
-					if (data[i] != files[id].data[ofs+i])
-					{
-						printf("read data from file %i does not match at ofs=%i read=%i but should be %i\n", id, ofs+i, data[i], files[id].data[ofs+i]);
-						exit(1);
-					}
-				}
-			break;
+        case 2: // read
+            printf("tid %1i %5i: read %i ofs=%i size=%i\n", tid, iter, id, ofs, newsize);
+            if (ofs+newsize > files[id].size) newsize = files[id].size - ofs - 1;
+            if (newsize < 0) newsize = 0;
+            retsize = pread(files[id].fd, data, newsize, ofs);
+            if (retsize < 0)
+            {
+                perror("Error: ");
+                exit(1);
+            }
+            assert(retsize == newsize);
+            for(int i=0; i<newsize; i++)
+            {
+                if (data[i] != files[id].data[ofs+i])
+                {
+                    printf("read data from file %i does not match at ofs=%i read=%i but should be %i\n", id, ofs+i, data[i], files[id].data[ofs+i]);
+                    exit(1);
+                }
+            }
+            break;
 
-			case 3: // check filesize
-				printf("%5i: filesize %i\n", iter, id);
-				if (fsize(files[id].fd) != files[id].size)
-				{
-					printf("size of file %i does not match %li %li\n", id, files[id].size, fsize(files[id].fd));
-					exit(1);
-				}
-			break;
-		}
+        case 3: // check filesize
+            printf("tid %1i %5i: filesize %i\n", tid, iter, id);
+            if (fsize(files[id].fd) != (unsigned int)files[id].size)
+            {
+                printf("size of file %i does not match %i %li\n", id, files[id].size, fsize(files[id].fd));
+                exit(1);
+            }
+            break;
+        }
 
-	}
-
-	for(int i=0; i<10; i++)
-	{
-		close(files[i].fd);
-	}
+        files[id].mtx.unlock();
+    }
+}
 
 
-	return 0;
+int main(int argc, char *argv[])
+{
+    unsigned int nthreads = 10;
+    if (argc != 3)
+    {
+        printf("Usage: %s nthreads niter\n", argv[0]);
+        return 1;
+    }
+    nthreads = atoi(argv[1]);
+    niter = atoi(argv[2]);
+    printf("number of threads: %i\n", nthreads);
+    printf("number of iterations per thread: %i\n", niter);
+    printf("number of files %i\n", nfiles);
+
+    for(unsigned int i=0; i<10; i++)
+    {
+        sprintf(files[i].filename, "tests%i.dat", i);
+        files[i].fd = open(files[i].filename, O_RDWR | O_CREAT | O_TRUNC);
+        files[i].size = 0;
+        memset(files[i].data, 0, MAXSIZE+1);
+    }
+
+    std::thread *t = new std::thread[nthreads];
+
+    for(unsigned int i=0; i<nthreads; i++)
+    {
+        t[i] = std::thread(Execute, i);
+    }
+
+    for(unsigned int i=0; i<nthreads; i++)
+    {
+        t[i].join();
+    }
+
+    for(int i=0; i<10; i++)
+    {
+        close(files[i].fd);
+    }
+
+    return 0;
 }
