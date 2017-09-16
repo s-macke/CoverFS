a3dfcd9f679ed1012068964a7f7d90f3bfb23822
Sebastian Macke <sebastian@macke.de>
2017-09-16 10:26:31 +0200

    Encrypt: Change to scrypt, prepare for XTS, better error handling

--- src/CEncrypt.cpp
+++ src/CEncrypt.cpp
@@ -18,31 +18,46 @@ typedef struct
     uint8_t salt[32];
     struct {
         char username[128];
-        uint8_t key[32];
-        uint8_t enccheckbytes[32];
-        uint8_t checkbytes[32];
+        uint8_t key[64];
+        uint8_t enccheckbytes[64];
+        uint8_t checkbytes[64];
         int32_t hashreps;
     } user[4];
 
 } TEncHeader;
 
+void GCryptCheckError(const char *function, gpg_error_t ret)
+{
+    if (ret)
+    {
+        fprintf (stderr, "%s: Failure: %s/%s\n",
+               function,
+               gcry_strsource (ret),
+               gcry_strerror (ret));
+        exit(1);
+    }
+}
 
-void CEncrypt::PassToHash(const std::string &message, uint8_t salt[32], uint8_t passkey[32], int hashreps)
+
+void CEncrypt::PassToHash(const std::string &message, uint8_t salt[32], uint8_t passkey[64], int hashreps)
 {
     char *pass = getpass(message.c_str());
-    gpg_error_t ret = gcry_kdf_derive( pass, strlen(pass), 
-    GCRY_KDF_PBKDF2, GCRY_MD_SHA256,
-    salt, 32, hashreps, 
-    32, passkey);
+    gpg_error_t ret = gcry_kdf_derive(
+    pass, strlen(pass),
+    GCRY_KDF_SCRYPT,
+    GCRY_MD_SHA256,
+    salt, 32,
+    hashreps,
+    64, passkey);
     memset(pass, 0, strlen(pass));
-    assert(ret == 0);
+    GCryptCheckError("getpass", ret);
 }
 
 void CEncrypt::CreateEnc(int8_t *block)
 {
     printf("Create Encryption block\n");
-    uint8_t key[32];
-    gcry_randomize (key, 32, GCRY_STRONG_RANDOM);
+    uint8_t key[64];
+    gcry_randomize (key, 64, GCRY_STRONG_RANDOM);
 
     TEncHeader *h = (TEncHeader*)block;
     memset(h, 0, sizeof(blocksize));
@@ -52,24 +67,26 @@ void CEncrypt::CreateEnc(int8_t *block)
 
     gcry_create_nonce (h->salt, 32);
 
-    h->user[0].hashreps = 1000;
+    h->user[0].hashreps = 500;
     strcpy(h->user[0].username, "poke");
-    gcry_create_nonce (h->user[0].enccheckbytes, 32);
+    gcry_create_nonce (h->user[0].enccheckbytes, 64);
     
-    uint8_t passkey[32];
+    uint8_t passkey[64];
     PassToHash("Set Password for filesystem: ", h->salt, passkey, h->user[0].hashreps); 
     gpg_error_t ret;
 
     gcry_cipher_hd_t hd;
     ret =  gcry_cipher_open(&hd, GCRY_CIPHER_AES256, GCRY_CIPHER_MODE_ECB, 0);
-    assert(ret == 0);
+    GCryptCheckError("gcry_cipher_open", ret);
     ret = gcry_cipher_setkey(hd, passkey, 32);
-    assert(ret == 0);
+    GCryptCheckError("gcry_cipher_setkey", ret);
     ret = gcry_cipher_encrypt (hd, 
-    h->user[0].checkbytes, 32, 
-    h->user[0].enccheckbytes, 32);
-    ret = gcry_cipher_encrypt (hd, h->user[0].key, 32, key, 32);
-    for(int i=0; i<32; i++) key[i] = 0x0;
+    h->user[0].checkbytes, 64,
+    h->user[0].enccheckbytes, 64);
+    GCryptCheckError("gcry_cipher_encrypt", ret);
+    ret = gcry_cipher_encrypt (hd, h->user[0].key, 64, key, 64);
+    GCryptCheckError("gcry_cipher_encrypt", ret);
+    for(int i=0; i<64; i++) key[i] = 0x0;
     gcry_cipher_close(hd);
 
     gcry_md_hash_buffer(GCRY_MD_CRC32, &h->crc, (int8_t*)h+4, blocksize-4);
@@ -77,14 +94,21 @@ void CEncrypt::CreateEnc(int8_t *block)
 
 CEncrypt::CEncrypt(CAbstractBlockIO &bio)
 {
-    assert(sizeof(TEncHeader) == 4+8+4+32+(128+32+32+32+4)*4);
+    assert(sizeof(TEncHeader) == 4+8+4+32+(128+64+64+64+4)*4);
     assert(bio.blocksize >= 1024);
     blocksize = bio.blocksize;
     gpg_error_t ret;
 
+    const char* gcryptversion = gcry_check_version (GCRYPT_VERSION);
+    if (gcryptversion == NULL)
+    {
+        fprintf(stderr, "Error: gcrypt version too old\n");
+        exit(1);
+    }
+    printf("gcrypt version %s\n", gcryptversion);
     ret = gcry_control (GCRYCTL_SET_THREAD_CBS, &gcry_threads_pthread);
-    gcry_check_version (NULL);
-    gcry_control (GCRYCTL_INITIALIZATION_FINISHED, 0);
+    GCryptCheckError("gcry_control", ret);
+    gcry_control(GCRYCTL_INITIALIZATION_FINISHED, 0);
 
     assert(gcry_md_get_algo_dlen (GCRY_MD_CRC32) == 4);
 
@@ -103,37 +127,37 @@ CEncrypt::CEncrypt(CAbstractBlockIO &bio)
     assert(h->majorversion == 1);
     assert(h->minorversion == 0);
 
-    uint8_t passkey[32];
+    uint8_t passkey[64];
     PassToHash("Password: ", h->salt, passkey, h->user[0].hashreps); 
 
     gcry_cipher_hd_t hd;
     ret =  gcry_cipher_open(&hd, GCRY_CIPHER_AES256, GCRY_CIPHER_MODE_ECB, 0);
-    assert(ret == 0);
+    GCryptCheckError("gcry_cipher_open", ret);
     ret = gcry_cipher_setkey(hd, passkey, 32);
-    assert(ret == 0);
-    uint8_t check[32];
-    ret = gcry_cipher_encrypt(hd, check, 32, h->user[0].enccheckbytes, 32);
-    assert(ret == 0);
+    GCryptCheckError("gcry_cipher_setkey", ret);
+    uint8_t check[64];
+    ret = gcry_cipher_encrypt(hd, check, 64, h->user[0].enccheckbytes, 64);
+    GCryptCheckError("gcry_cipher_encrypt", ret);
 
-    if (memcmp(check, h->user[0].checkbytes, 32) != 0)
+    if (memcmp(check, h->user[0].checkbytes, 64) != 0)
     {
         fprintf(stderr, "Error: Cannot decrypt filesystem. Did you type the right password?\n");
         exit(1);
     }
-    uint8_t key[32];
-    ret = gcry_cipher_decrypt(hd, key, 32, h->user[0].key, 32);
-    assert(ret == 0);
+    uint8_t key[64];
+    ret = gcry_cipher_decrypt(hd, key, 64, h->user[0].key, 64);
+    GCryptCheckError("gcry_cipher_decrypt", ret);
     gcry_cipher_close(hd);
 
     for(int i=0; i<4; i++)
     {
         ret =  gcry_cipher_open(&hdblock[i], GCRY_CIPHER_AES256, GCRY_CIPHER_MODE_CBC, 0);
-        assert(ret == 0);
+        GCryptCheckError("gcry_cipher_open", ret);
         ret = gcry_cipher_setkey(hdblock[i], key, 32);
-        assert(ret == 0);
+        GCryptCheckError("gcry_cipher_setkey", ret);
     }
 
-    memset(key, 0, 32);
+    memset(key, 0, 64);
     memset(block, 0, blocksize);
 }
 
@@ -150,7 +174,7 @@ void CEncrypt::Decrypt(const int blockidx, int8_t *d)
         if (!mutex[i].try_lock()) continue;
         gcry_cipher_setiv (hdblock[i], iv, 16);
         gpg_error_t ret = gcry_cipher_decrypt(hdblock[i], d, blocksize, NULL, 0);
-        assert(ret == 0);
+        GCryptCheckError("gcry_cipher_decrypt", ret);
         mutex[i].unlock();
         return;
     }
@@ -159,7 +183,7 @@ void CEncrypt::Decrypt(const int blockidx, int8_t *d)
     mutex[0].lock();
     gcry_cipher_setiv (hdblock[0], iv, 16);
     gpg_error_t ret = gcry_cipher_decrypt(hdblock[0], d, blocksize, NULL, 0);
-    assert(ret == 0);
+    GCryptCheckError("gcry_cipher_decrypt", ret);
     mutex[0].unlock();
 }
 
@@ -175,7 +199,7 @@ void CEncrypt::Encrypt(const int blockidx, int8_t* d)
         if (!mutex[i].try_lock()) continue;
         gcry_cipher_setiv (hdblock[i], iv, 16);
         gpg_error_t ret = gcry_cipher_encrypt(hdblock[i], d, blocksize, 0, 0);
-        assert(ret == 0);
+        GCryptCheckError("gcry_cipher_encrypt", ret);
         mutex[i].unlock();
         return;
     }
@@ -184,6 +208,6 @@ void CEncrypt::Encrypt(const int blockidx, int8_t* d)
     mutex[1].lock();
     gcry_cipher_setiv (hdblock[1], iv, 16);
     gpg_error_t ret = gcry_cipher_encrypt(hdblock[1], d, blocksize, 0, 0);
-    assert(ret == 0);
+    GCryptCheckError("gcry_cipher_encrypt", ret);
     mutex[1].unlock();
 }
